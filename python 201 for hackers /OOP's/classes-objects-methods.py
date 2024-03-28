1c8595aaefa9b7c88a5653d0716f485d44b843ca
singhggithub <112242974+singhggithub@users.noreply.github.com>
2024-03-29 04:29:30 +0530

    Add files via upload

---
+++ python 201 for hackers /OOP's/classes-objects-methods.py
@@ -0,0 +1,91 @@
+class Person:
+    'Person base class'
+    wants_to_hack = True
+
+    def __init__(self,name,age) :
+        self.name = name
+        self.age = age
+
+    def print_name(self):
+        print("My name is {}".format(self.name))
+
+    def print_age(self):
+        print("My age is {}".format(self.age))
+
+    def birthday(self):
+        self.age += 1
+
+bob = Person("bob",30)
+alice = Person("alice",20)
+mallory = Person("mallory",50)
+
+
+bob.print_name()
+alice.print_name()
+mallory.print_name()
+
+
+bob.print_age()
+alice.print_age()
+mallory.print_age()
+
+bob.age = 31
+bob.print_age()
+
+bob.birthday()
+bob.print_age()
+
+
+print(bob)
+print(alice)
+print(mallory)
+
+print(bob.name)
+print(bob.age)
+
+print(hasattr(bob,"age"))
+print(hasattr(bob,"asd"))
+
+
+print(getattr(bob,"age"))
+
+setattr(bob, "asd",100)
+
+print(getattr(bob,"asd"))
+delattr(bob,"asd")
+
+#print(getattr(bob,"asd"))
+
+print(Person.wants_to_hack)
+print(bob.wants_to_hack)
+print(alice.wants_to_hack)
+print(mallory.wants_to_hack)
+
+Person.wants_to_hack = "No way!"
+
+print(Person.wants_to_hack)
+print(bob.wants_to_hack)
+print(alice.wants_to_hack)
+print(mallory.wants_to_hack)
+
+bob.wants_to_hack = "sure!"
+
+print(Person.wants_to_hack)
+print(bob.wants_to_hack)
+print(alice.wants_to_hack)
+print(mallory.wants_to_hack)
+
+bob.print_name()
+del bob.name
+#bob.print_name()
+
+#del Person
+#print(alice.name)
+
+bob2 = Person("bob2",35)
+
+print(Person.__dict__)
+print(Person.__doc__)
+print(Person.__name__)
+print(Person.__module__)
+
